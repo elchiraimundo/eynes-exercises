5e8adcf9fbe940de5118dbc4090137f1978de46e
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-29 22:10:06 -0300

    Creo la lista usando la libreria random

--- simple/simple.py
+++ simple/simple.py
@@ -1,9 +1,15 @@
 import random
 
-
+# Creo la lista de diccionarios con la funcion random
 def simple_list():
-    pass
-
+    lista_de_diccionarios = []
+    for i in range(10):
+        diccionario = {
+            "id": i + 1,
+            "age": random.randint(1, 100) 
+        }
+        lista_de_diccionarios.append(diccionario)
+    return lista_de_diccionarios
 
 def sort_list():
     pass
