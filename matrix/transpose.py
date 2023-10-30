d575f7a09f34e634da4c1fc19833b53c17168a2c
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-29 22:06:20 -0300

    Creo datos necesarios de la tabla

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,12 @@
 def transpose(matrix):
-    pass
+    
+    # Guardo las filas y columnas 
+    filas = len(matrix)
+    columnas = len(matrix[0])
+
+    # Creo una matriz con datos [0] con tantas columnas como fila tenga y viceversa
+    matriz_transpuesta = []
+    for _ in range(columnas):
+        fila = [0] * filas
+        matriz_transpuesta.append(fila)
+
